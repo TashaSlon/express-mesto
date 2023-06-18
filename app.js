5c29c57ec0e298464069395783c663f56cc20c88
plyusnina <plyusnina@live.ru>
2023-06-19 00:58:44 +0500

    null route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routerUsers = require('./routes/users');
 const routerCards = require('./routes/cards');
+const process = require('process');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -23,6 +24,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/users', routerUsers);
 app.use('/cards', routerCards);
+app.use('/*', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
 
 app.listen(PORT, () => {
 });
